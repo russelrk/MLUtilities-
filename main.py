37fb604cc2a39c7841ea263d84b0a03b52288b02
russelrk <49132622+russelrk@users.noreply.github.com>
2023-09-22 11:05:31 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import argparse
+import numpy as np
+
+# Your function `classification_results` should be imported if it's in a different file or you can also define it in this file.
+from .get_score import classification_results
+
+def test_classification_results(n: int, target_value: float, sen: bool, youden_index: bool, display_cm: bool):
+    """
+    Generate random binary classification data and compute classification metrics based on the given parameters to test the module.
+    
+    Args:
+    n (int): Number of samples per class.
+    target_value (float): Target specificity or sensitivity value.
+    sen (bool): Use sensitivity as the target value if True, otherwise specificity.
+    youden_index (bool): Use Youden's Index to determine threshold if True.
+    display_cm (bool): Display the confusion matrix if True.
+
+    Returns:
+    None
+    """
+    np.random.seed(0)
+
+    y_true = np.array([0] * n + [1] * n)
+
+    # Generate scores for each class using normal distributions
+    y_pred = np.array([np.random.normal(loc=0.3, scale=0.3) for _ in range(n)] + 
+                      [np.random.normal(loc=0.7, scale=0.3) for _ in range(n)])
+
+    results = classification_results(y_true, y_pred, youden_index=youden_index, target_value=target_value, sen=sen, display_cm=display_cm)
+    print(results)
+
+def main():
+    """Prints 'Done' to indicate the end of script execution."""
+    print('Done')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Compute classification metrics.")
+    parser.add_argument('--n', type=int, default=500, help="Number of samples per class.")
+    parser.add_argument('--target_value', type=float, default=0.9, help="Target specificity or sensitivity value.")
+    parser.add_argument('--sen', action='store_true', help="Use sensitivity as target value if set, otherwise specificity.")
+    parser.add_argument('--youden_index', action='store_true', help="Use Youden's Index to determine threshold.")
+    parser.add_argument('--display_cm', action='store_true', help="Display the confusion matrix.")
+
+    args = parser.parse_args()
+    
+    test_classification_results(args.n, args.target_value, args.sen, args.youden_index, args.display_cm)
+    main()
